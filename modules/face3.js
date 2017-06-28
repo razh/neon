b8affeb77e7657c9e5c8ac1920dffad58a553ced
razh <somecrazyidiot@gmail.com>
2017-06-27 23:24:44 -0400

    Additional Geometry transform methods
    - Remove .normal property from Face3.
    - Add Face3#clone() method.

--- modules/face3.js
+++ modules/face3.js
@@ -6,20 +6,28 @@ export type Face3 = {
   a: number,
   b: number,
   c: number,
-  normal: Vector3,
   color: Vector3,
   vertexColors: Array<Vector3>,
 };
 
-import { vec3_create } from './vec3';
+import { vec3_create, vec3_clone } from './vec3';
 
 export function face3_create(a: number, b: number, c: number): Face3 {
   return {
     a: a,
     b: b,
     c: c,
-    normal: vec3_create(),
     color: vec3_create(),
     vertexColors: [],
+  };
+}
+
+export function face3_clone(face: Face3) {
+  return {
+    a: face.a,
+    b: face.b,
+    c: face.c,
+    color: vec3_clone(face.color),
+    vertexColors: face.vertexColors.map(vec3_clone),
   };
 }
--- modules/geom.js
+++ modules/geom.js
@@ -8,15 +8,15 @@ export type Geometry = {
   faces: Array<Face3>,
 };
 
-import { face3_create } from './face3';
-
 import {
   vec3_create,
+  vec3_set,
+  vec3_clone,
   vec3_copy,
-  vec3_cross,
-  vec3_subVectors,
-  vec3_normalize,
+  vec3_add,
+  vec3_multiply,
 } from './vec3';
+import { face3_create, face3_clone } from './face3';
 
 export function geom_create(): Geometry {
   return {
@@ -50,4 +50,67 @@ export function geom_push(geom: Geometry, vertices: Array<number>, faces: Array<
   }
 
   return geom;
+}
+
+export var geom_translate = (function() {
+  var vector = vec3_create();
+
+  return function(geom: Geometry, x: number, y: number, z: number) {
+    vec3_set(vector, x, y, z);
+
+    geom.vertices.map(function(vertex) {
+      vec3_add(vertex, vector);
+    });
+
+    return geom;
+  };
+}());
+
+export var geom_scale = (function() {
+  var vector = vec3_create();
+
+  return function(geom: Geometry, x: number, y: number, z: number) {
+    vec3_set(vector, x, y, z);
+
+    geom.vertices.map(function(vertex) {
+      vec3_multiply(vertex, vector);
+    });
+
+    return geom;
+  };
+}());
+
+export function geom_merge(a: Geometry, b: Geometry) {
+  var vertexOffset = a.vertices.length;
+
+  var i;
+  for (i = 0; i < b.vertices.length; i++) {
+    a.vertices.push(vec3_clone(b.vertices[i]));
+  }
+
+  for (i = 0; i < b.faces.length; i++) {
+    var face = b.faces[i];
+    var faceCopy = face3_create(
+      face.a + vertexOffset,
+      face.b + vertexOffset,
+      face.c + vertexOffset
+    );
+
+    vec3_copy(faceCopy.color, face.color);
+
+    for (var j = 0; j < face.vertexColors.length; j++) {
+      faceCopy.vertexColors.push(vec3_clone(face.vertexColors[j]));
+    }
+
+    a.faces.push(faceCopy);
+  }
+
+  return a;
+}
+
+export function geom_clone(geom: Geometry) {
+  var clone = geom_create();
+  clone.vertices = geom.vertices.map(vec3_clone);
+  clone.faces = geom.faces.map(face3_clone);
+  return clone;
 }
