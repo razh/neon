7ebf792a28f56ad6dbf5f2529d4ac21286c082e0
razh <somecrazyidiot@gmail.com>
2017-06-28 23:37:51 -0400

    Add MeshPhongMaterial properties

--- modules/material.js
+++ modules/material.js
@@ -1,3 +1,23 @@
 // @flow
 
-export type Material = {};
+import type { Vector3 } from './vec3';
+
+export type Material = {
+  color: Vector3,
+  specular: Vector3,
+  shininess: number,
+  emissive: Vector3,
+};
+
+import { vec3_create } from './vec3';
+
+// MeshPhongMaterial.
+export function material_create(): Material {
+  return {
+    color: vec3_create(1, 1, 1),
+    // 0x111111
+    specular: vec3_create(1 / 15, 1 / 15, 1 / 15),
+    shininess: 30,
+    emissive: vec3_create(),
+  };
+}
