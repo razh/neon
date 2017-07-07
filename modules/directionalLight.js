12289bf39ec1e9aaa0c49bb327ca76ec39e3f24a
razh <somecrazyidiot@gmail.com>
2017-07-07 00:07:55 -0400

    Replace Light with DirectionalLight

--- modules/directionalLight.js
+++ modules/directionalLight.js
@@ -3,21 +3,26 @@
 import type { Object3D } from './object3d';
 import type { Vector3 } from './vec3';
 
-export type Light = Object3D & {
+export type DirectionalLight = Object3D & {
   color: Vector3,
   intensity: number,
+  target: Object3D,
 };
 
 import { object3d_create } from './object3d';
 import { vec3_create } from './vec3';
 
-export function light_create(): Light {
+export function light_create(
+  color: Vector3 = vec3_create(),
+  intensity: number = 1,
+): DirectionalLight {
   return Object.assign(
     {},
     object3d_create(),
     {
-      color: vec3_create(),
-      intensity: 1,
+      color,
+      intensity,
+      target: object3d_create(),
     }
   );
 }
