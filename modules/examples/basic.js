e01ca410531190a9c449c2f5aa14698559d7b6f2
razh <somecrazyidiot@gmail.com>
2017-08-07 23:02:57 -0400

    Extract basic example code

---
+++ modules/examples/basic.js
@@ -0,0 +1,52 @@
+// @flow
+
+import {
+  createShaderProgram,
+  createFloat32Buffer,
+  setFloat32Attribute,
+  setMat4Uniform,
+  getAttributeLocations,
+  getUniformLocations,
+} from '../shader';
+
+import vert from '../shaders/test_vert.glsl';
+import frag from '../shaders/test_frag.glsl';
+
+export var render = (gl: WebGLRenderingContext) => () => {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  var program = createShaderProgram(gl, vert, frag);
+
+  var attributes = getAttributeLocations(gl, program);
+  var uniforms = getUniformLocations(gl, program);
+
+  gl.useProgram(program);
+
+  var position = new Float32Array([
+    -0.5, -0.5, 0,
+    0.5, -0.5, 0,
+    -0.5, 0.5, 0,
+    0.5, 0.5, 0,
+  ]);
+
+  var positionBuffer = createFloat32Buffer(gl, position);
+
+  var color = createFloat32Buffer(gl, new Float32Array([
+    1, 1, 1,
+    1, 0, 0,
+    0, 1, 0,
+    0, 0, 1,
+  ]));
+
+  setMat4Uniform(gl, uniforms.M, [
+    0.5, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0.25, 0, 0, 1,
+  ]);
+
+  setFloat32Attribute(gl, attributes.p, positionBuffer, 3);
+  setFloat32Attribute(gl, attributes.c, color, 3);
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, position.length / 3);
+};
--- modules/index.js
+++ modules/index.js
@@ -2,16 +2,7 @@
 
 import { camera_create } from './camera';
 import { boxGeom_create } from './boxGeom';
-import {
-  createShaderProgram,
-  createFloat32Buffer,
-  setFloat32Attribute,
-  setMat4Uniform,
-  getAttributeLocations,
-  getUniformLocations,
-} from './shader';
-import vert from './shaders/test_vert.glsl';
-import frag from './shaders/test_frag.glsl';
+import { render as renderBasic } from './examples/basic';
 
 camera_create();
 boxGeom_create(1, 1, 1);
@@ -32,44 +23,7 @@ var setSize = (width, height) => {
   gl.viewport(0, 0, width, height);
 };
 
-var render = () => {
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  var program = createShaderProgram(gl, vert, frag);
-
-  var attributes = getAttributeLocations(gl, program);
-  var uniforms = getUniformLocations(gl, program);
-
-  gl.useProgram(program);
-
-  var position = new Float32Array([
-    -0.5, -0.5, 0,
-    0.5, -0.5, 0,
-    -0.5, 0.5, 0,
-    0.5, 0.5, 0,
-  ]);
-
-  var positionBuffer = createFloat32Buffer(gl, position);
-
-  var color = createFloat32Buffer(gl, new Float32Array([
-    1, 1, 1,
-    1, 0, 0,
-    0, 1, 0,
-    0, 0, 1,
-  ]));
-
-  setMat4Uniform(gl, uniforms.M, [
-    0.5, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0,
-    0.25, 0, 0, 1,
-  ]);
-
-  setFloat32Attribute(gl, attributes.p, positionBuffer, 3);
-  setFloat32Attribute(gl, attributes.c, color, 3);
-
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, position.length / 3);
-};
+var render = renderBasic(gl);
 
 setSize(window.innerWidth, window.innerHeight);
 render();
