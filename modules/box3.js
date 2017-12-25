2a1e658e485a01d2f8b267813bddd024dfc7ab72
razh <somecrazyidiot@gmail.com>
2017-12-25 01:23:02 -0500

    Port THREE.Ray and Box3 for mesh intersections

---
+++ modules/box3.js
@@ -0,0 +1,97 @@
+// @flow
+
+import type { Mesh } from './mesh';
+import type { Object3D } from './object3d';
+import type { Vector3 } from './vec3';
+
+export type Box3 = {
+  min: Vector3,
+  max: Vector3,
+};
+
+import { object3d_traverse, object3d_updateMatrixWorld } from './object3d';
+import {
+  vec3_create,
+  vec3_add,
+  vec3_applyMatrix4,
+  vec3_min,
+  vec3_max,
+} from './vec3';
+
+export var box3_create = (
+  min: Vector3 = vec3_create(Infinity, Infinity, Infinity),
+  max: Vector3 = vec3_create(-Infinity, -Infinity, -Infinity),
+) => {
+  return {
+    min,
+    max,
+  };
+};
+
+export var box3_copy = (a: Box3, b: Box3) => {
+  Object.assign(a.min, b.min);
+  Object.assign(a.max, b.max);
+  return a;
+};
+
+export var box3_makeEmpty = (box: Box3) => {
+  box.min.x = box.min.y = box.min.z = Infinity;
+  box.max.x = box.max.y = box.max.z -= Infinity;
+  return box;
+};
+
+export var box3_expandByPoint = (box: Box3, point: Vector3) => {
+  vec3_min(box.min, point);
+  vec3_max(box.max, point);
+  return box;
+};
+
+export var box3_setFromPoints = (box: Box3, points: Vector3[]) => {
+  box3_makeEmpty(box);
+  points.map(point => box3_expandByPoint(box, point));
+  return box;
+};
+
+export var box3_setFromObject = (() => {
+  var v1 = vec3_create();
+
+  return (box: Box3, object: Object3D) => {
+    object3d_updateMatrixWorld(object);
+    box3_makeEmpty(box);
+
+    object3d_traverse(object, node => {
+      var { geometry } = ((node: any): Mesh);
+      if (geometry) {
+        geometry.vertices.map(vertex => {
+          Object.assign(v1, vertex);
+          vec3_applyMatrix4(v1, node.matrixWorld);
+          box3_expandByPoint(box, v1);
+        });
+      }
+    });
+
+    return box;
+  };
+})();
+
+export var box3_containsPoint = (box: Box3, point: Vector3) => {
+  return (
+    box.min.x <= point.x && point.x <= box.max.x &&
+    box.min.y <= point.y && point.y <= box.max.y &&
+    box.min.z <= point.z && point.z <= box.max.z
+  );
+};
+
+export var box3_intersectsBox = (a: Box3, b: Box3) => {
+  return !(
+    a.max.x < b.min.x || a.min.x > b.max.x ||
+    a.max.y < b.min.y || a.min.y > b.max.y ||
+    a.max.z < b.min.z || a.min.z > b.max.z
+  );
+};
+
+export var box3_translate = (box: Box3, offset: Vector3) => {
+  vec3_add(box.min, offset);
+  vec3_add(box.max, offset);
+  return box;
+};
---
+++ modules/ray.js
@@ -0,0 +1,212 @@
+// @flow
+
+import type { Box3 } from './box3';
+import type { Face3 } from './face3';
+import type { Matrix4 } from './mat4';
+import type { Mesh } from './mesh';
+import type { Vector3 } from './vec3';
+
+export type Ray = {
+  origin: Vector3,
+  direction: Vector3,
+};
+
+export type Intersection = {
+  object: Mesh,
+  distance: number,
+  point: Vector3,
+  face?: Face3,
+  faceIndex?: number,
+};
+
+import { mat4_create, mat4_getInverse } from './mat4';
+import {
+  vec3_create,
+  vec3_add,
+  vec3_applyMatrix4,
+  vec3_clone,
+  vec3_cross,
+  vec3_crossVectors,
+  vec3_distanceTo,
+  vec3_dot,
+  vec3_multiplyScalar,
+  vec3_subVectors,
+  vec3_transformDirection,
+} from './vec3';
+
+export var ray_create = (
+  origin: Vector3 = vec3_create(),
+  direction: Vector3 = vec3_create(),
+): Ray => {
+  return {
+    origin,
+    direction,
+  };
+};
+
+export var ray_copy = (a: Ray, b: Ray) => {
+  Object.assign(a.origin, b.origin);
+  Object.assign(a.direction, b.direction);
+  return a;
+};
+
+export var ray_at = (ray: Ray, t: number, result: Vector3 = vec3_create()) => {
+  return vec3_add(
+    vec3_multiplyScalar(Object.assign(result, ray.direction), t),
+    ray.origin,
+  );
+};
+
+export var ray_intersectBox = (ray: Ray, box: Box3, optionalTarget?: Vector3) => {
+  var { origin, direction } = ray;
+
+  var txmin = (box.min.x - origin.x) / direction.x;
+  var txmax = (box.max.x - origin.x) / direction.x;
+  if (txmin > txmax) {
+    [txmin, txmax] = [txmax, txmin];
+  }
+
+  var tymin = (box.min.y - origin.y) / direction.y;
+  var tymax = (box.max.y - origin.y) / direction.y;
+  if (tymin > tymax) {
+    [tymin, tymax] = [tymax, tymin];
+  }
+
+  if ((txmin > tymax) || (tymin > txmax)) {
+    return;
+  }
+
+  // Math.min/max with NaN support (0 / 0).
+  var tmin = ((tymin > txmin) || (txmin !== txmin)) ? tymin : txmin;
+  var tmax = ((tymax < txmax) || (txmax !== txmax)) ? tymax : txmax;
+
+  var tzmin = (box.min.z - origin.z) / direction.z;
+  var tzmax = (box.max.z - origin.z) / direction.z;
+  if (tzmin > tzmax) {
+    [tzmin, tzmax] = [tzmax, tzmin];
+  }
+
+  if ((tmin > tzmax) || (tzmin > tmax)) {
+    return;
+  }
+
+  tmin = ((tzmin > tmin) || (tmin !== tmin)) ? tzmin : tmin;
+  tmax = ((tzmax < tmax) || (tmax !== tmax)) ? tzmax : tmax;
+
+  if (tmax < 0) {
+    return;
+  }
+
+  return ray_at(ray, tmin >= 0 ? tmin : tmax, optionalTarget);
+};
+
+export var ray_intersectTriangle = (() => {
+  var diff = vec3_create();
+  var edge1 = vec3_create();
+  var edge2 = vec3_create();
+  var normal = vec3_create();
+
+  return (ray: Ray, a: Vector3, b: Vector3, c: Vector3, optionalTarget?: Vector3) => {
+    vec3_subVectors(edge1, b, a);
+    vec3_subVectors(edge2, c, a);
+
+    vec3_crossVectors(normal, edge1, edge2);
+
+    // Determinant.
+    var DdN = vec3_dot(ray.direction, normal);
+    var sign = 1;
+
+    if (DdN > 0) {
+      return;
+    } if (DdN < 0) {
+      sign = -1;
+      DdN *= -1;
+    } else {
+      return;
+    }
+
+    vec3_subVectors(diff, ray.origin, a);
+    var DdQxE2 = sign * vec3_dot(ray.direction, vec3_crossVectors(edge2, diff, edge2));
+    if (DdQxE2 < 0) {
+      return;
+    }
+
+    var DdE1xQ = sign * vec3_dot(ray.direction, vec3_cross(edge1, diff));
+    if (DdE1xQ < 0) {
+      return;
+    }
+
+    if ((DdQxE2 + DdE1xQ) > DdN) {
+      return;
+    }
+
+    var QdN = -sign * vec3_dot(diff, normal);
+    if (QdN < 0) {
+      return;
+    }
+
+    return ray_at(ray, QdN / DdN, optionalTarget);
+  };
+})();
+
+export var ray_intersectsMesh = (() => {
+  var inverseMatrix = mat4_create();
+  var rayCopy = ray_create();
+
+  var intersectionPoint = vec3_create();
+  var intersectionPointWorld = vec3_create();
+
+  var checkIntersection = (object, ray, a, b, c, point): ?Intersection => {
+    var intersect = ray_intersectTriangle(ray, a, b, c);
+    if (!intersect) {
+      return;
+    }
+
+    Object.assign(intersectionPointWorld, point);
+    vec3_applyMatrix4(intersectionPointWorld, object.matrixWorld);
+
+    var distance = vec3_distanceTo(ray.origin, intersectionPointWorld);
+
+    return {
+      object,
+      distance,
+      point: vec3_clone(intersectionPointWorld),
+    };
+  };
+
+  return (ray: Ray, object: Mesh) => {
+    var intersections = [];
+
+    mat4_getInverse(inverseMatrix, object.matrixWorld);
+    ray_applyMatrix4(ray_copy(rayCopy, ray), inverseMatrix);
+
+    var { vertices, faces } = object.geometry;
+
+    faces.map((face, index) => {
+      var a = vertices[face.a];
+      var b = vertices[face.b];
+      var c = vertices[face.c];
+
+      var intersection = checkIntersection(object, rayCopy, a, b, c, intersectionPoint);
+      if (intersection) {
+        intersection.face = face;
+        intersection.faceIndex = index;
+        intersections.push(intersection);
+      }
+    });
+
+    return intersections;
+  };
+})();
+
+export var ray_applyMatrix4 = (r: Ray, m: Matrix4) => {
+  vec3_applyMatrix4(r.origin, m);
+  vec3_transformDirection(r.direction, m);
+  return r;
+};
+
+export var ray_intersectObjects = (ray: Ray, objects: Mesh[]) => {
+  return []
+    .concat(...objects.map(object => ray_intersectsMesh(ray, object)))
+    .sort((a, b) => a.distance - b.distance);
+};
