6e6822176a3700ad00ae15ce9b9cc3c52397c82a
razh <somecrazyidiot@gmail.com>
2019-08-19 01:48:20 -0400

    shadowMesh

--- src/shadowMesh.js
+++ src/shadowMesh.js
@@ -4,10 +4,19 @@
  * @typedef {import('./vec3').Vector3} Vector3
  */
 
-import { mat4_create } from './mat4.js';
+/**
+ * @typedef ShadowMeshSubclass
+ * @property {Mesh} mesh
+ */
+
+ /**
+ * @typedef {Mesh & ShadowMeshSubclass} ShadowMesh
+ */
+
+import { mat4_create, mat4_multiplyMatrices } from './mat4.js';
 import { material_create } from './material.js';
 import { mesh_create } from './mesh.js';
-import { vec3_clone, vec3_setScalar, vec3_Y } from './vec3.js';
+import { vec3_clone, vec3_dot, vec3_setScalar, vec3_Y } from './vec3.js';
 
 var shadowMatrix = mat4_create();
 var normal = vec3_clone(vec3_Y);
@@ -19,18 +28,52 @@ shadowMaterial.shininess = 0;
 
 /**
  * @param {Mesh} mesh
- * @return {Mesh}
+ * @return {ShadowMesh}
  */
-export var shadowMesh = mesh => {
-  return mesh_create(mesh.geometry, shadowMaterial);
+export var shadowMesh_create = mesh => {
+  return {
+    ...mesh_create(mesh.geometry, shadowMaterial),
+    mesh,
+  };
 };
 
 // amount of light-ray divergence. Ranging from:
 // 0.001 = sunlight(min divergence) to 1.0 = pointlight(max divergence)
 // must be slightly greater than 0, due to 0 causing matrixInverse errors
 /**
- * @param {Mesh} shadowMesh
+ * @param {ShadowMesh} shadowMesh
  * @param {Vector3} lightPosition
  * @param {number} w
  */
-export var shadowMesh_update = (shadowMesh, lightPosition, w = 0.001) => {};
+export var shadowMesh_update = (shadowMesh, lightPosition, w = 0.001) => {
+  var { y } = shadowMesh.position;
+
+  // based on https://www.opengl.org/archives/resources/features/StencilTalk/tsld021.htm
+  var dot = vec3_dot(normal, lightPosition) - y * w;
+
+  shadowMatrix[0] = dot - lightPosition.x * normal.x;
+  shadowMatrix[4] = -lightPosition.x * normal.y;
+  shadowMatrix[8] = -lightPosition.x * normal.z;
+  shadowMatrix[12] = -lightPosition.x * -y;
+
+  shadowMatrix[1] = -lightPosition.y * normal.x;
+  shadowMatrix[5] = dot - lightPosition.y * normal.y;
+  shadowMatrix[9] = -lightPosition.y * normal.z;
+  shadowMatrix[13] = -lightPosition.y * -y;
+
+  shadowMatrix[2] = -lightPosition.z * normal.x;
+  shadowMatrix[6] = -lightPosition.z * normal.y;
+  shadowMatrix[10] = dot - lightPosition.z * normal.z;
+  shadowMatrix[14] = -lightPosition.z * -y;
+
+  shadowMatrix[3] = -w * normal.x;
+  shadowMatrix[7] = -w * normal.y;
+  shadowMatrix[11] = -w * normal.z;
+  shadowMatrix[15] = dot - w * -y;
+
+  mat4_multiplyMatrices(
+    shadowMesh.matrixWorld,
+    shadowMatrix,
+    shadowMesh.mesh.matrixWorld,
+  );
+};
